7c4cdeefdf65f54d273967f35016e70f660e919f
Bitna <2bn3244@gmail.com>
2021-01-19 20:56:43 +0900

    DayOfJava

---
+++ 02Day/src/Sample02_variable.java
@@ -0,0 +1,37 @@
+
+public class Sample02_variable {
+
+	public static void main(String[] args) {
+		
+		//홍길동 20 서울 성별(남) 결혼여부(false) 키(185.63) 체중(78.25)
+		//1. 변수선언(메모리에 특정크기의 영역생성)
+		
+		String name;
+		int age;
+		String address;
+		char gender;
+		boolean isMarried;
+		float height;
+		double weight;
+				
+		//2. 변수에 값 초기화(값을 맨처음 설정하는 작업) -사용은 반드시 초기화 후에 함
+		name = "홍길동";
+		age = 20;
+		address = "서울";
+		gender = '남';
+		isMarried = false;
+		height = 185.63f;
+		weight = 78.25;
+		
+		//3. 사용 - 출력
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(address);
+		System.out.println(gender);
+		System.out.println(isMarried);
+		System.out.println(height);
+		System.out.println(weight);
+
+	}
+
+}
---
+++ 02Day/src/Sample02_variable2.java
@@ -0,0 +1,36 @@
+
+public class Sample02_variable2 {
+
+	public static void main(String[] args) {
+		
+		//선언과 동시에 변수명 지정
+		
+		String name = "홍길동";
+		int age = 20;
+		String address = "서울";
+		char gender = '남';
+		boolean isMarried = false;
+		float height = 185.63f;
+		double weight = 78.25;
+		
+		//2. 사용 출력
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(address);
+		System.out.println(gender);
+		System.out.println(isMarried);
+		System.out.println(height);
+		System.out.println(weight);
+		
+		//3. printf 사용
+		
+		System.out.printf("이름은 %s, 나이는 %d, 주소는 %s 입니다.", name,age,address);
+		System.out.println();
+		
+		//4. println으로 모든 값 - > "문자열 + 변수명 == 문자열 생성 
+		System.out.println("이름은 " + name + ", 나이는 " + age +", 주소는 " + address + ", 성별은 " + gender + ", 결혼은 " + isMarried +
+				", 키는 " + height + ", 몸무게는 " + weight + " 입니다.");
+
+	}
+
+}
